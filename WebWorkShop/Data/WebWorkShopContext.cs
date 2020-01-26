9cd50a680371f7c6d4e991e76ca1d488937d1e9b
Luiz <luizfsilva06@outlook.com>
2020-01-26 10:42:51 -0300

    crud

---
+++ WebWorkShop/Data/WebWorkShopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebWorkShop.Models
+{
+    public class WebWorkShopContext : DbContext
+    {
+        public WebWorkShopContext (DbContextOptions<WebWorkShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebWorkShop.Models.Department> Department { get; set; }
+    }
+}
