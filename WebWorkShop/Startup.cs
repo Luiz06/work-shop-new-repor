4ae38a860fb9bb55c265e8a2a9cfde3e996dcafd
Luiz <luizfsilva06@outlook.com>
2020-01-26 13:13:45 -0300

    Banco de dados

--- WebWorkShop/Startup.cs
+++ WebWorkShop/Startup.cs
@@ -37,7 +37,8 @@ namespace WebWorkShop
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebWorkShopContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebWorkShopContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebWorkShopContext"), builder => 
+                    builder.MigrationsAssembly("WebWorkShop")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
